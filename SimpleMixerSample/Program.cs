a64c5add496cf21684bfc1cd82fe5ac5d87704b1
Bogdan Panchuk <bogdan.panchuck@intapp.com>
2018-12-19 11:47:11 +0200

    Improved Demo application.

--- SimpleMixerSample/Program.cs
+++ SimpleMixerSample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace ResonatorBeta
@@ -10,30 +12,72 @@ namespace ResonatorBeta
         {
             using (var player = new ResonatorPlayer())
             {
+                var processor = new ResonatorProcessor();
+                player.Use(processor);
+
                 OpenFileDialog openFileDialog = new OpenFileDialog
                 {
                     Title = "Select adaptive bundle config file",
                     Filter = "Resonator Bundle Config | *.amb"
                 };
 
-                while (openFileDialog.ShowDialog() != DialogResult.OK) ;
-
-                var processor = new ResonatorProcessor();
-                processor.Import(openFileDialog.FileName);
-
-                player.Use(processor);
-                player.Play();
-
-                Console.WriteLine(processor.CurrentState);
-
-                foreach (var state in processor.States)
+                bool paused = false;
+                while (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    Console.ReadLine();
-                    processor.CurrentState = state;
-                    Console.WriteLine(processor.CurrentState);
-                }
+                    processor.Import(openFileDialog.FileName);
 
-                Console.ReadLine();
+                    player.Play();
+
+                    Console.WriteLine("Choose states: ");
+
+                    var states = new Dictionary<int, string>();
+                    int i = 1;
+                    foreach (var state in processor.States)
+                    {
+                        Console.WriteLine("\t[" + i + "] " + state);
+                        states[i++] = state;
+                    }
+
+                    var currentState = processor.CurrentState;
+
+                    Console.WriteLine("\n[0] Pause/Resume");
+                    Console.WriteLine("\nCurrent state: " + processor.CurrentState);
+
+                    int lastStateKey = states.FirstOrDefault(x => x.Value == processor.DefaultState).Key;
+                    while (true)
+                    {
+                        int key = 0;
+                        do
+                        {
+                            var input = Console.ReadKey(true);
+                            key = (input.KeyChar - '0');
+                        }
+                        while (key > states.Count);
+
+                        if (key == 0) {
+                            if (paused)
+                            {
+                                player.Resume();
+                                currentState = processor.CurrentState;
+                            }
+                            else
+                            {
+                                player.Pause();
+                                paused = true;
+                                currentState = "paused";
+                            }
+                            key = lastStateKey;
+                        }
+                        else
+                        {
+                            lastStateKey = key;
+                            processor.CurrentState = states[key];
+                            currentState = processor.CurrentState;
+                        }
+
+                        Console.WriteLine("Current state: " + currentState);
+                    }
+                }
             }
         }
     }
